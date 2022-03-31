63c4014f4c41b1309ec1e0e2e34f59eacf69c7e9
kaanb1ack <89489424+kaanb1ack@users.noreply.github.com>
2022-03-31 15:35:33 -0700

    tutorial 2.4 complete

--- Assets/Course Library/Script/DestroyOutOfBounds.cs
+++ Assets/Course Library/Script/DestroyOutOfBounds.cs
@@ -7,11 +7,6 @@ public class DestroyOutOfBounds : MonoBehaviour
     private float topBound = 30;
     private float lowerBound = -10;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -22,7 +17,8 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
-            Destroy(gameObject);  
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Course Library/Script/PlayerController.cs
+++ Assets/Course Library/Script/PlayerController.cs
@@ -10,12 +10,7 @@ public class PlayerController : MonoBehaviour
 
     public GameObject projectilePrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
